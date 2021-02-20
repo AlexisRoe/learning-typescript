beee7c75fdd4bbed5d7555b52c135539cc410249
Alexis Roehrling <A.Roehrling@gmx.de>
2021-02-20 16:11:06 +0100

    Add another project

--- src/projects/RickAndMorty/constants.ts
+++
@@ -1,8 +0,0 @@
-import { initState } from "./interfaces";
-
-export const initialState: initState = {
-    episodes: [],
-    favourites: [],
-};
-
-export const URL = "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes";
