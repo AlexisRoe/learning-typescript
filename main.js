6d2ddf6e3f9b1bedaa7c99806cfd2ff0f71bd576
Alexis Roehrling <A.Roehrling@gmx.de>
2021-02-20 16:39:21 +0100

    combine witch typescript.config

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// the ! means that I´m sure as a developer that the selected element exists, because ts can`t access the html
-// ... as HTMLInputElement tells ts compiler what kind of methods are available
+"use strict";
 var button = document.querySelector("button");
 var input1 = document.getElementById("num1");
 var input2 = document.getElementById("num2");
@@ -8,7 +7,6 @@ function addInput(input1, input2) {
     var result = Number(input1) + Number(input2);
     return result.toString();
 }
-// input.values are always type string -> you have to convert it first
 button.addEventListener("click", function () {
     output.innerText = "Result: " + addInput(input1.value, input2.value);
 });
